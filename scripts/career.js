471ad7e47a547a7791dd2a9a26400c5f49cd9749
ibrahim ahmad <ibrahimahmad.info@gmail.com>
2024-07-13 09:55:58 +0800

    Move script to external js file to make it easy to maintain. otherwise we have to go to each html page to check if we have any scripts element.

---
+++ scripts/career.js
@@ -0,0 +1,52 @@
+// Start of header related functions ----------------------------
+
+function toggleMenu() {
+  const nav = document.querySelector("nav ul");
+  if (nav.style.display === "flex") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "flex";
+  }
+}
+
+// end of Header related functions----------------------------
+
+function hideCV() {
+  const cvElement = document.querySelector(".cv");
+  cvElement.style.display = "none";
+}
+
+function showCV() {
+  const toggleSwitch = document.querySelector("#toggleSwitch");
+  const cvElement = document.querySelector(".cv");
+
+  // select all non-CV sections
+  const introElement = document.querySelector(".intro");
+  const skillsElement = document.querySelector(".key-skills");
+  const employmentElement = document.querySelector(".employment");
+  const techElement = document.querySelector(".technologies");
+
+  // if checked: hide all non-cv section, display cv section
+  if (toggleSwitch.checked) {
+    cvElement.style.display = "";
+
+    introElement.style.display = "none";
+    skillsElement.style.display = "none";
+    employmentElement.style.display = "none";
+    techElement.style.display = "none";
+  }
+
+  // if un-checked: display non-cv section, hide cv section
+  if (!toggleSwitch.checked) {
+    cvElement.style.display = "none";
+
+    introElement.style.display = "";
+    skillsElement.style.display = "";
+    employmentElement.style.display = "";
+    techElement.style.display = "";
+  }
+}
+document.getElementById("toggleSwitch").addEventListener("change", showCV);
+
+//hide cv during page-load
+hideCV();
