315c2087cfd8d27b9cd55fbd776bce0c0b978901
ibrahim ahmad <ibrahimahmad.info@gmail.com>
2024-07-14 21:42:48 +0800

    Reorganized project folders for webpack configuration

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-// Header related functions
-function toggleMenu() {
-  const nav = document.querySelector("nav ul");
-  if (nav.style.display === "flex") {
-    nav.style.display = "none";
-  } else {
-    nav.style.display = "flex";
-  }
-}
