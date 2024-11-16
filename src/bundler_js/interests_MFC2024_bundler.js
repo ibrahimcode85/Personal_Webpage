f5c0d65e78f58154c52d31dbdf420c5db1093642
ibrahim ahmad <ibrahimahmad.info@gmail.com>
2024-11-16 22:02:23 +0800

    Update youtube embedded video link.

--- src/bundler_js/interests_MFC2024_bundler.js
+++ src/bundler_js/interests_MFC2024_bundler.js
@@ -8,7 +8,7 @@ import "../scripts/main.js";
 import img1 from "C:\\Users\\ibrah\\OneDrive\\Pictures\\Swimming\\2024_11_MFC_FreeDiving\\ef40bb82-cf50-49e6-be3f-493e52266c39.jpg";
 import img2 from "C:\\Users\\ibrah\\OneDrive\\Pictures\\Swimming\\2024_11_MFC_FreeDiving\\a7cba825-ed23-4b03-a6da-a48eb49a5018.jpg";
 import img3 from "C:\\Users\\ibrah\\OneDrive\\Pictures\\Swimming\\2024_11_MFC_FreeDiving\\622e0a3a-06ab-4b83-8a47-63d4b340967a.jpg";
-import img4 from "C:\\Users\\ibrah\\OneDrive\\Pictures\\Swimming\\2024_11_MFC_FreeDiving\\31d6c541-d82d-4a0f-bb84-445359371ad1.jpg";
+import img4 from "C:\\Users\\ibrah\\OneDrive\\Pictures\\Swimming\\2024_11_MFC_FreeDiving\\IMG_2075_jpg.jpg";
 
 // Setting image sources for interests_langkawiSwimFest.html
 document
@@ -20,6 +20,4 @@ document
 document
   .getElementById("622e0a3a-06ab-4b83-8a47-63d4b340967a")
   .setAttribute("src", img3);
-document
-  .getElementById("31d6c541-d82d-4a0f-bb84-445359371ad1")
-  .setAttribute("src", img4);
+document.getElementById("IMG_2075_jpg").setAttribute("src", img4);
