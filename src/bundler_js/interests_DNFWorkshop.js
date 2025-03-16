cc04ef8d8c40746dc26f7b2b24ece75586a5adbe
ibrahim ahmad <ibrahimahmad.info@gmail.com>
2025-03-16 17:20:53 +0800

    Add new interest section on dnf workshop.

---
+++ src/bundler_js/interests_DNFWorkshop.js
@@ -0,0 +1,17 @@
+// interestsSub_bundler.js
+import "../styles/styles.css";
+import "../styles/interests_sub_pages.css";
+import "../styles/variables.css";
+import "../scripts/main.js";
+
+// Imports for interests_langkawiSwimFest.html
+import img1 from "C:\\Users\\ibrah\\OneDrive\\Pictures\\Swimming\\2024_12_17_DNF_Workshop_Penang\\f1ac2bd2ad211b23745a073df3dc3d01.jpg";
+import img2 from "C:\\Users\\ibrah\\OneDrive\\Pictures\\Swimming\\2024_12_17_DNF_Workshop_Penang\\813d84d793940ada3d610c4b36d8cdeb.jpg";
+
+// Setting image sources for interests_langkawiSwimFest.html
+document
+  .getElementById("f1ac2bd2ad211b23745a073df3dc3d01")
+  .setAttribute("src", img1);
+document
+  .getElementById("813d84d793940ada3d610c4b36d8cdeb")
+  .setAttribute("src", img2);
