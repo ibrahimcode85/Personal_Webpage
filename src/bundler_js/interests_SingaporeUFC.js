a375c690313631956467f8d23f8bcc75dafce29e
ibrahim ahmad <ibrahimahmad.info@gmail.com>
2025-05-11 16:14:09 +0800

    Add new card for singapore UFC and corresponding js bundler file.

---
+++ src/bundler_js/interests_SingaporeUFC.js
@@ -0,0 +1,13 @@
+// interestsSub_bundler.js
+import "../styles/styles.css";
+import "../styles/interests_sub_pages.css";
+import "../styles/variables.css";
+import "../scripts/main.js";
+
+// Imports for interests_langkawiSwimFest.html
+import img1 from "C:\\Users\\ibrah\\OneDrive\\Pictures\\Swimming_Freediving\\2025_05_FreeDiving_UFC_Singapore\\IMG_7148_jpg.jpg";
+import img2 from "C:\\Users\\ibrah\\OneDrive\\Pictures\\Swimming_Freediving\\2025_05_FreeDiving_UFC_Singapore\\IMG_0043_jpg.jpg";
+
+// Setting image sources for interests_langkawiSwimFest.html
+document.getElementById("IMG_7148_jpg").setAttribute("src", img1);
+document.getElementById("IMG_0043_jpg").setAttribute("src", img2);
